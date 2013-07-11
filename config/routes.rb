0457b7f147111b62d527ef1e0819131ba5c7ecc4
Gene <genealston@gmail.com>
2013-07-11 09:28:47 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+get "users/new"
+#get "static_pages/home"
+#get "static_pages/help"
+#get "static_pages/about"
+#get "static_pages/contact"
 
-  get "static_pages/help"
+root :to =>"static_pages#home"
+match '/signup', :to=>'users#new'
+match '/help', :to=>'static_pages#help'
+match '/about', :to=>'static_pages#about'
+match '/contact', :to=>'static_pages#contact'
 
-  get "static_pages/about"
+#match '/', :to=>'static_pages#home'
 
-  get "static_pages/contact"
+#match '/static_pages/help' => 'static_pages#help', :via => :get
+#match '/static_pages/about' =>  'static_pages#about', :via => :get
+#match '/static_pages/contact' => 'static_pages#contact', :via => :get
+#match '/static_pages/home' => 'static_pages#home', :via => :get
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/support/utilities.rb
@@ -0,0 +1,10 @@
+	def full_title (page_title)
+		#method definition
+		base_title = "Ruby on Rails Tutorial Sample App"
+		#variable assignment
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
