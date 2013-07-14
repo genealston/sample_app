edfd780afb750a031a962a4353721db0cb484cd0
Gene <genealston@gmail.com>
2013-07-14 14:35:38 -0700

    Make a basic User model (including secure passwords)

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,12 @@
-	def full_title (page_title)
+include ApplicationHelper
+
+#	def full_title (page_title)
 		#method definition
-		base_title = "Ruby on Rails Tutorial Sample App"
+#		base_title = "Ruby on Rails Tutorial Sample App"
 		#variable assignment
-		if page_title.empty?
-			base_title
-		else
-			"#{base_title} | #{page_title}"
-		end
-	end
+#		if page_title.empty?
+#			base_title
+#		else
+#			"#{base_title} | #{page_title}"
+#		end
+#	end
