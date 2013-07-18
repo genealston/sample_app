18a3fb2317988da8ced337e35fb994ceaefe10c6
Gene <genealston@gmail.com>
2013-07-17 20:44:08 -0700

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -108,6 +108,33 @@ end
 		it {should be_invalid}
 	end
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email){"Foo@ExAMPle.com"}
+		it "should be saved as all lower-case" do
+			@user.email=mixed_case_email
+			@user.save
+			@user.reload.email.should==mixed_case_email.downcase
+		end
+	end
 end
+
+#describe "User pages" do
+
+#	subject {page}
+
+#	describe "signup page" do
+
+#		before {visit signup_path}
+#		it {should have_selector('h1',:text=>'Sign up')}
+#		it {should have_selector('title',:text=>'Sign up')}
+#	end
+#end
+
+#	describe "profile page" do
+
+#		before {visit user_path(user)}
+#		it {should have_selector('h1',:text=>user.name)}
+#		it {should have_selector('title',:text=>user.name)}
+#	end
 #  pending "add some examples to (or delete) #{__FILE__}"
 
