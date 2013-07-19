82489c2ee4bdac82a163561e65a72af65299f066
Gene <genealston@gmail.com>
2013-07-19 15:40:31 -0700

    sign up enhancements

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,6 +25,10 @@ subject { page }
  				expect { click_button submit }.not_to change(User,:count)
  			end
  		end
+ 		describe "after invalid submission" do
+ 			before { click_button submit}
+ 			it {should have_content('error')}
+ 		end 		
  		
  		describe "with valid information" do
  			before do
@@ -32,6 +36,13 @@ subject { page }
  				fill_in "Email", :with=>"user@example.com"
  				fill_in "Password", :with=>"foobar"
  				fill_in "Confirmation", :with=>"foobar"
+ 			end
+
+ 			describe "after saving the user" do
+ 				before {click_button submit}
+ 				let(:user) {User.find_by_email('user@example.com')}
+ 				it {should have_selector('title',text:user.name)}
+ 				it {should have_selector('div.alert.alert-success',text:'Welcome')}
  			end
 
  			it "should create a user" do
